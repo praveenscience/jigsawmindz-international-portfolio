8dcd8a0e963205e9deb979741b3ad940590213f7
Praveen Kumar <praveenscience@gmail.com>
2021-03-28 14:30:43 +0100

    Added CSS using SCSS.

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,18 @@ import Header from "./Header";
 import Config from "../constants/config.json";
 
 class App extends Component {
+  state = {
+    Dark: true
+  };
   componentDidMount() {
     document.title = Config.Name + "'s Portfolio";
   }
-
   render() {
     return (
       <div className="App">
-        <Header dark={true}>{Config.Name}</Header>
+        <Header className="Header" dark={this.state.Dark}>
+          {Config.Name}
+        </Header>
       </div>
     );
   }
