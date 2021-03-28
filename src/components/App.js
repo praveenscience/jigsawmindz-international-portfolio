aa1096bdca2fa6d3b39654369ad403b18686590b
Praveen Kumar <praveenscience@gmail.com>
2021-03-28 14:16:43 +0100

    Imported Config File.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import Config from "../constants/config.json";
 
 class App extends Component {
   render() {
-    return <div className="App">Hey Jigsies</div>;
+    return (
+      <div className="App">
+        Hey Jigsies
+        <pre className="border rounded bg-light p-2">
+          {JSON.stringify(Config, null, 2)}
+        </pre>
+      </div>
+    );
   }
 }
 
